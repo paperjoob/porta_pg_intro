42e8d61b1bcc81f8ad967b80f7659ea274229ad8
See Yang <seeyang24@gmail.com>
2019-09-11 14:38:05 -0500

    dom updates

--- server/routes/music_router.js
+++ server/routes/music_router.js
@@ -2,39 +2,6 @@ const express = require('express');
 const pool = require('../modules/pool.js'); // direct path -- .. brings you to the next level
 const router = express.Router();
 
-let musicLibrary = [
-    {
-        artist: "Sturgill Simpson",
-        track: "Keep It Between the Lines",
-        rank: 1,
-        published: "4-16-2016"
-    },
-    {
-        artist: "Margo Price",
-        track: "Since You Put Me Down",
-        rank: 2,
-        published: "3-25-2016"
-    },
-    {
-        artist: "Jason Isbell",
-        track: "Alabama Pines",
-        rank: 3,
-        published: "4-12-2011"
-    },
-    {
-        artist: "Midland",
-        track: "Check Cashin' Country",
-        rank: 4,
-        published: "9-22-2017"
-    },
-    {
-        artist: "Nikki Lane",
-        track: "Highway Queen",
-        rank: 5,
-        published: "2-17-2017"
-    }
-]
-
 router.get('/', (req, res) => {
     let queryText = `SELECT * FROM "songs";`; // the inner semi-colon ends the query;
     pool.query(queryText) // passing the queryText variable into the pool query;
@@ -42,12 +9,25 @@ router.get('/', (req, res) => {
         // console.log('results: ', result);
         res.send(result.rows);
     })
+    .catch( (error) => {
+        console.log('error making query', error);
+        res.sendStatus(500);
+    })
 });
 
 router.post('/', (req, res) => {
     console.log("HELLO FROM THE POST", req.body);
-    res.sendStatus(200);
-    musicLibrary.push(req.body);
+    const newSong = req.body;
+    const queryText = `INSERT INTO "songs" ("artist", "track", "rank", "published")
+    VALUES ('${newSong.artist}', '${newSong.track}', ${newSong.rank}, '${newSong.published}') ;`; // use backticks
+    pool.query(queryText).then( (result) => {
+        console.log('results: ', result);
+        res.sendStatus(201);
+    })
+    .catch( (error) => {
+        console.log(`error making query ${queryText}`, error);
+        res.sendStatus(500);
+    })
 })
 
 module.exports = router;
